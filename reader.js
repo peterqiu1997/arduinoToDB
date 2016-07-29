3522234b4e86e0be8bd85aaab746057520f10411
Peter Qiu <peterqiu1997@gmail.com>
2016-07-29 11:15:19 -0700

    added twilio

--- reader.js
+++ reader.js
@@ -3,13 +3,24 @@ const mongoose = require('mongoose');
 const model = require('./ArduinoModel');
 const sp = require('serialport');
 
-const uristring = 'URL';
+// read from arduino
+const uristring = '***REMOVED***';
 const SerialPort = sp.SerialPort;
-const serialport = new SerialPort("/dev/ttyACM0", {
+const serialport = new SerialPort("/dev/cu.usbmodem1411", {
     parser: SerialPort.parsers.readline('\n')
 });
+
+// set up twilio CHANGE THIS BEFORE COMMITTING
+const accountSid = '***REMOVED***';
+const authToken = '***REMOVED***';
+const client = require('twilio')(accountSid, authToken);
+const NUMBERS = ['510-789-3847']; //'408-693-6790', '408-832-5830', 
+const FROM_NUMBER = '650-285-1722';
+
 const saveInterval = 1000;
-let newPoint = {};
+let newPoint = {},
+    uploading = false,
+    lastCall = 0;
 
 mongoose.connect(uristring, function(err, res) {
     if (err) {
@@ -23,7 +34,10 @@ serialport.on('open', function(){
 
 const handleData = function(data) {
     const dataArray = data.split(',');
+    console.log(dataArray);
     if (dataArray.length === 6) {
+        checkCall(dataArray[5]);
+        uploading = true;
         newPoint = new model({
             pressure: dataArray[0],       
             temperature: dataArray[1],
@@ -33,12 +47,33 @@ const handleData = function(data) {
     }
 }
 
+const checkCall = function(count) {
+    const now = Date.now();
+    if (count >= 0.07 && now > (lastCall + 300000)) {
+        for (let i = 0; i < NUMBERS.length; i += 1) {
+            client.messages.create({
+                to: NUMBERS[i],
+                from: FROM_NUMBER,
+                body: 'somethings afoot in the cleanroom - sent at: ' + 
+                       new Date(now).toString().split(' ').slice(0, 4).join(' ')
+            }, function (err, message) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+        lastCall = now;
+    }
+}
+
 const save = setInterval(function() {
-    newPoint.save(function(err) {
+    if (uploading) {
+        newPoint.save(function(err) {
             if (err) { 
                 console.log(err);
             }
         });
+    }
 }, saveInterval);
 
 serialport.on('close', function() { clearInterval(save); });
